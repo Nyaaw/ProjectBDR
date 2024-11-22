026507c1d54b2e20864ffd2d4b43856369c63735
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2024-11-22 10:50:57 +0100

    add base struct sql

--- phase3/script.sql
+++ phase3/script.sql
@@ -1,1 +1,73 @@
+/* Création de la base de données*/
+DROP DATABASE IF EXIST projet;
+CREATE DATABASE projet;
+
+/*Création de tables*/
+CREATE TABLE createur(
+
+);
+
+CREATE TABLE groupe(
+
+);
+
+CREATE TABLE personne(
+
+);
+
+CREATE TABLE genre(
+
+);
+
+CREATE TABLE Type(
+
+);
+
+CREATE TABLE media(
+
+);
+
+CREATE TABLE papier(
+
+);
+
+
+CREATE TABLE livre(
+
+);
+
+CREATE TABLE bd(
+
+);
+
+
+CREATE TABLE numerique(
+
+);
+
+
+CREATE TABLE film(
+
+);
+
+
+CREATE TABLE serie(
+
+);
+
+CREATE TABLE jeuvideo(
+
+);
+
+CREATE TABLE utilisateur(
+
+);
+
+CREATE TABLE liste(
+
+);
+
+CREATE TABLE mediautilisateur(
+    
+);
 
