fe71de624bcffa45e02e606d71a4ca60247a7c28
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2024-12-20 11:54:08 +0100

    add media page and footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,5 +1,6 @@
 
     <footer>
+        Projet BDR 2024-2025
     </footer>
 </body>
 </html>
---
+++ src/media.php
@@ -0,0 +1,33 @@
+<?php include_once("includes/header.php"); ?>
+
+<main>
+    <h1>titre</h1>
+    <div id="media">
+        <div class="media_section1">
+            <div>
+            <h3>Réalisateur</h3>
+            <p>NOM</p>
+            </div>
+            <h3>Genres</h3>
+            <ul>
+                <li>Genre 1</li>
+                <li>Genre 2</li>
+                <li>Genre 3</li>
+            </ul>
+            <!--
+            <h3>Options specifique</h3>
+            <ul>
+                <li>option 1: attribut</li>
+            </ul>
+-->
+        </div>
+        <div class="media_section2">
+            <h2>Description</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus consequat iaculis. Nullam pulvinar nisi ac urna congue, in mattis leo ullamcorper. Suspendisse ut dapibus tellus. Integer non nibh in turpis mollis porta id vitae massa. Vivamus lorem dui, rutrum vel vestibulum ut, vehicula et lorem. Suspendisse auctor, justo eu finibus aliquam, massa tellus malesuada est, in pretium ipsum justo vel ex. Maecenas in faucibus libero. Vivamus ullamcorper placerat justo, sed laoreet lectus cursus id. Nunc id semper leo. Ut nisl ante, iaculis at urna ut, commodo finibus urna. Nullam vehicula sagittis eros ac suscipit. Nullam a nibh ante.
+
+Curabitur lacus dolor, dictum nec faucibus in, pellentesque imperdiet velit. Integer aliquam nunc at nulla accumsan tempor. Ut finibus facilisis cursus. Etiam lobortis leo dolor, non ultrices dolor sodales a. Nam at felis et justo sodales faucibus eget non felis. Aenean mi dolor, blandit nec neque eu, rhoncus mollis enim. Duis in scelerisque odio, ultricies vulputate ex. Pellentesque maximus, sem sed lacinia iaculis, ante elit rutrum dui, eget rhoncus ipsum quam at libero. Quisque tincidunt neque urna, vitae vehicula augue sagittis vel. Quisque vel tincidunt neque, ut tristique nibh. Aliquam rutrum sollicitudin quam sed ultrices. Sed sodales fringilla mi vitae dapibus. Duis hendrerit a est sed auctor. Cras non nulla et quam tincidunt sagittis. Ut consectetur ipsum ultricies nisi congue egestas. Cras sollicitudin diam ac eros dignissim, sed cursus erat ultrices. </p>
+        </div>
+    </div>
+</main>
+
+<?php include_once("includes/footer.php"); ?>
