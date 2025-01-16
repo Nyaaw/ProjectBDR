d91c2118f2a7a4dbdab3b0c9971bf8a7e8a0e19a
Nyaaw <cactus318@hotmail.fr>
2025-01-16 19:11:23 +0100

    added some params in endpoints

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,6 +1,8 @@
 package ch.heigvd.dai;
 
 import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
 import io.javalin.rendering.template.JavalinThymeleaf;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -43,6 +45,8 @@ public class Main {
         });
         
         app.get("/list", ctx -> {
+            String name = getQueryParam(ctx, "name");
+
             ctx.render("list.html", Map.of("genres", genres));
         });
         
@@ -55,19 +59,78 @@ public class Main {
         });
         
         app.get("/media", ctx -> {
+            String idParam = getQueryParam(ctx, "id");
+            Integer id = checkForNumericParam(ctx, idParam);
+
             ctx.render("media.html", Map.of("genres", genres));
+        });
+
+        app.post("/media/addtolist", ctx -> {
+
+            String idParam = getQueryParam(ctx, "id");
+            Integer id = checkForNumericParam(ctx, idParam);
+
+            // add to List
+
+            ctx.redirect("/media?id=" + id);
+
+        });
+
+        app.post("/comment", ctx -> {
+
+            String idParam = getQueryParam(ctx, "id");
+            Integer id = checkForNumericParam(ctx, idParam);
+
+            // comment
+
+            ctx.redirect("/media?id=" + id);
+
         });
         
         app.get("/mylists", ctx -> {
+            String idParam = getQueryParam(ctx, "id");
+            Integer id = checkForNumericParam(ctx, idParam);
+
             ctx.render("mylists.html", Map.of("genres", genres));
         });
-        
+
         app.get("/result", ctx -> {
+            String search = getQueryParam(ctx, "search");
+
             ctx.render("result.html", Map.of("genres", genres));
         });
-        
+
+        app.error(404, ctx -> ctx.render("error.html",
+                Map.of("errorCode", "404", "errorMsg", "Cette page n'existe pas !")));
+
+        app.error(400, ctx -> ctx.render("error.html",
+                Map.of("errorCode", "400", "errorMsg", "Paramètre invalide")));
 
 
         app.start(PORT);
     }
+
+    private static String getQueryParam(Context ctx, String param) {
+        String value = ctx.queryParam(param);
+        if (value == null) {
+            throw new BadRequestResponse();
+        }
+
+        return value;
+    }
+
+    private static Integer checkForNumericParam(Context ctx, String param) {
+
+        Integer num = null;
+        try {
+
+            num = Integer.parseInt(param);
+
+        } catch (NumberFormatException e) {
+            throw new BadRequestResponse();
+        }
+
+        return num;
+    }
+
 }
