0fe99f75a991b633984039e974eb4d0c7591c2dd
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2025-01-18 22:20:54 +0100

    deplace fonction insert media

--- docs/BDR-phases/phase4/base_struct_and_data.sql
+++ docs/BDR-phases/phase4/base_struct_and_data.sql
@@ -29,6 +29,80 @@ CREATE TRIGGER after_user_insert
     FOR EACH ROW
     EXECUTE FUNCTION create_default_lists();
 
+-- insert media
+DROP FUNCTION IF EXISTS insert_media(TEXT, DATE, TEXT, TEXT, INTEGER, BOOLEAN, INTEGER, INTEGER, TEXT, TEXT, INTEGER);
+
+CREATE OR REPLACE FUNCTION insert_media(
+    media_nom TEXT,
+    media_dateSortie DATE DEFAULT CURRENT_DATE,
+    media_description TEXT DEFAULT NULL,
+    media_type TEXT DEFAULT NULL,
+    nb_pages INTEGER DEFAULT NULL,
+    bd_couleur BOOLEAN DEFAULT NULL,
+    film_duree TIME DEFAULT NULL,
+    serie_nbSaisons INTEGER DEFAULT NULL,
+    jeu_video_type TEXT DEFAULT NULL,
+    genre_nom TEXT DEFAULT NULL,
+    createur_id INTEGER DEFAULT NULL
+) RETURNS VOID AS $$
+DECLARE
+id_media INTEGER;
+BEGIN
+    -- Étape 1 : Insérer dans la table Media
+INSERT INTO Media (nom, dateSortie, description)
+VALUES (media_nom, media_dateSortie, media_description)
+    RETURNING id INTO id_media;
+
+-- Étape 2 : Insérer dans la sous-catégorie correspondante
+IF media_type = 'Livre' THEN
+            INSERT INTO Papier (id) VALUES (id_media);
+INSERT INTO Livre (id, nbPage) VALUES (id_media, nb_pages);
+
+ELSIF media_type = 'BD' THEN
+            INSERT INTO Papier (id) VALUES (id_media);
+INSERT INTO BD (id, couleur) VALUES (id_media, bd_couleur);
+
+ELSIF media_type = 'Film' THEN
+            INSERT INTO Numerique (id) VALUES (id_media);
+INSERT INTO Film (id, duree) VALUES (id_media, film_duree);
+
+ELSIF media_type = 'Série' THEN
+            INSERT INTO Numerique (id) VALUES (id_media);
+INSERT INTO Serie (id, nbSaison) VALUES (id_media, serie_nbSaisons);
+
+ELSIF media_type = 'Jeu Vidéo' THEN
+            INSERT INTO Numerique (id) VALUES (id_media);
+INSERT INTO JeuVideo (id) VALUES (id_media);
+
+-- Associer au type de jeu vidéo (Type.nom est la clé primaire)
+IF jeu_video_type IS NOT NULL THEN
+                    INSERT INTO JeuVideo_Type (jeuVideoId, typeNom)
+                    VALUES (id_media, jeu_video_type);
+END IF;
+END IF;
+
+    -- Étape 3 : Associer au genre (Genre.nom est la clé primaire)
+    IF genre_nom IS NOT NULL THEN
+        -- Vérifier si le genre existe déjà dans la table Genre
+        PERFORM 1 FROM Genre WHERE nom = genre_nom;
+        -- Si le genre n'existe pas, l'ajouter
+        IF NOT FOUND THEN
+            INSERT INTO Genre (nom) VALUES (genre_nom);
+END IF;
+
+        -- Insérer dans Media_Genre
+INSERT INTO Media_Genre (mediaId, genreNom)
+VALUES (id_media, genre_nom);
+END IF;
+
+    -- Étape 4 : Associer au créateur
+    IF createur_id IS NOT NULL THEN
+        INSERT INTO Media_Createur (mediaId, createurId)
+        VALUES (id_media, createur_id);
+END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 
 
 /*Views*/
