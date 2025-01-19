11d85581a6e35fabae854b66012ad65cfd34a3ae
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2025-01-19 22:30:55 +0100

    maybe fix

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -41,7 +41,7 @@ public class Main {
         });
 
 
-        Connection conn = DriverManager.getConnection("jdbc:postgresql://db:5666/mediatheque", "postgres", "trustno1");
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://db/mediatheque", "postgres", "trustno1");
         DSLContext dsl = DSL.using(conn);
 
         MediaController.dsl = dsl;
